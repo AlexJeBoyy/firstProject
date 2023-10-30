5001331f88651f4a0540f57da6530d6539a7e985
AlexJeBoyy <a.strikwerda@sintlucasedu.nl>
2023-10-30 23:55:52 +0100

    yolo

---
+++ Assets/Scripts/BreakoutT/BallBreak.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBreak : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2D;
+
+    public float speed = 300;
+
+    private Vector2 velocity;
+
+
+    void Start()
+    {
+        velocity.x = Random.Range(-1f, 1f);
+        velocity.y = 1;
+        rigidbody2D.AddForce(velocity*speed);
+    }
+
+}
